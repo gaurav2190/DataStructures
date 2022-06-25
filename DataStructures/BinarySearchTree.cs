52552f052324bea6391493cd2ec2e1c604836252
Gaurav Khatri <gkhatri2190@gmail.com>
2022-06-25 14:37:53 +0530

    BST insertion

--- DataStructures/BinarySearchTree.cs
+++ DataStructures/BinarySearchTree.cs
@@ -19,5 +19,20 @@ namespace DataStructures
             
             return null;        
         }
+
+        public TreeNode InsertIntoBST(TreeNode root, int val) {
+            // check if the node passed as param is null then it is terminating condn return new object then.
+            if(root == null)
+                return new TreeNode(val);
+            
+            // if val is less then move to left subtree
+            if(val < root.val)
+                root.left = InsertIntoBST(root.left, val);
+            // else move to right subtree.
+            else if(val > root.val)
+                root.right = InsertIntoBST(root.right, val);
+            
+            return root;
+        }
     }
 }
